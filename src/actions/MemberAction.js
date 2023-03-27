2a33693fb7ac4acf4aa684eda7c3e1c52fa68fff
rajat-gith <rajatnayak1582002@gmail.com>
2023-03-27 20:10:40 +0530

    bug work on

--- src/actions/MemberAction.js
+++ src/actions/MemberAction.js
@@ -5,24 +5,22 @@ import {
   MEMBERS_LIST_SUCCESS,
 } from "../constants/MemberConstants";
 
-export const listMembers =
-  (year = "") =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: MEMBERS_LIST_REQUEST });
-      const { data } = await axios.get(`/api/members?year=${year}`);
+export const listMembers = (year) => async (dispatch) => {
+  try {
+    dispatch({ type: MEMBERS_LIST_REQUEST });
+    const { data } = await axios.get(`/api/members?year=${year}`);
 
-      dispatch({
-        type: MEMBERS_LIST_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: MEMBERS_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+    dispatch({
+      type: MEMBERS_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: MEMBERS_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
